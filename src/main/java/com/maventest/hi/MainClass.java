58d771a7e3cade5627933f2797495b4c2e3accea
nkhouse <932974672@qq.com>
2017-08-02 12:19:17 +0800

    first commie

---
+++ src/main/java/com/maventest/hi/MainClass.java
@@ -0,0 +1,14 @@
+package com.maventest.hi;
+
+/*
+ * @author Li Bob
+ * data: 2017/7/31
+ */
+public class MainClass {
+	public static void main(String[] args) {
+		
+		Meizi love = new Meizi();
+		love.run(1,30);				
+		System.out.println("Successfully End!");
+	}
+}
